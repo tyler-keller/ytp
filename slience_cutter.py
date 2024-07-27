68a2f080c555d5141c0beb3c7e9edfa5406755f3
Tyler Keller <tyler.c.keller@gmail.com>
2024-07-27 13:12:07 -0600

    reformatting silence cutter

--- slience_cutter.py
+++ slience_cutter.py
@@ -8,7 +8,6 @@ def find_silences(filename, dB = -35):
     returns a list:
       even elements (0,2,4, ...) denote silence start time
       uneven elements (1,3,5, ...) denote silence end time
-
   """
   command = ["ffmpeg", "-i", filename, "-af", "silencedetect=n=" + str (dB) + "dB:d=1", "-f", "null", "-"]
   output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -29,21 +28,15 @@ def find_silences(filename, dB = -35):
   #return silence_section_list
   return time_list
 
-
-
-def getVideoDuration(filename:str) -> float:
-  command = ["ffprobe","-i",filename,"-v","quiet",
-             "-show_entries","format=duration","-hide_banner",
-             "-of","default=noprint_wrappers=1:nokey=1"]
-
+def get_video_duration(filename:str) -> float:
+  command = ["ffprobe","-i",filename,"-v","quiet","-show_entries","format=duration","-hide_banner","-of","default=noprint_wrappers=1:nokey=1"]
   output = subprocess.run (command, stdout=subprocess.PIPE)
   s = str(output.stdout, "UTF-8")
   return float (s)
 
-def getSectionsOfNewVideo (silences, duration):
+def get_sections_of_new_video (silences, duration):
   """Returns timings for parts, where the video should be kept"""
   return [0.0] + silences + [duration]
-
 
 def ffmpeg_filter_getSegmentFilter(videoSectionTimings):
   ret = ""
@@ -55,107 +48,43 @@ def ffmpeg_filter_getSegmentFilter(videoSectionTimings):
   ret = ret[:-1]
   return ret
 
-def getFileContent_videoFilter(videoSectionTimings):
+def get_file_content_video_filter(videoSectionTimings):
   ret = "select='"
   ret += ffmpeg_filter_getSegmentFilter (videoSectionTimings)
   ret += "', setpts=N/FRAME_RATE/TB"
   return ret
 
-def getFileContent_audioFilter(videoSectionTimings):
+def get_file_content_audio_filter(videoSectionTimings):
   ret = "aselect='"
   ret += ffmpeg_filter_getSegmentFilter (videoSectionTimings)
   ret += "', asetpts=N/SR/TB"
   return ret
 
-def writeFile (filename, content):
+def write_file (filename, content):
   with open (filename, "w") as file:
     file.write (str(content))
 
-
-def ffmpeg_run (file, videoFilter, audioFilter, outfile):
+def ffmpeg_run(file, videoFilter, audioFilter, outfile):
   # prepare filter files
   vFile = tempfile.NamedTemporaryFile (mode="w", encoding="UTF-8", prefix="silence_video")
   aFile = tempfile.NamedTemporaryFile (mode="w", encoding="UTF-8", prefix="silence_audio")
-
   videoFilter_file = vFile.name #"/tmp/videoFilter" # TODO: replace with tempfile
   audioFilter_file = aFile.name #"/tmp/audioFilter" # TODO: replace with tempfile
-  writeFile (videoFilter_file, videoFilter)
-  writeFile (audioFilter_file, audioFilter)
-
+  write_file(videoFilter_file, videoFilter)
+  write_file(audioFilter_file, audioFilter)
   command = ["ffmpeg","-i",file,
               "-filter_script:v",videoFilter_file,
               "-filter_script:a",audioFilter_file,
               outfile]
   subprocess.run (command)
-
   vFile.close()
   aFile.close()
 
 
-
 def cut_silences(infile, outfile, dB = -35):
-  print ("detecting silences")
-  silences = findSilences (infile,dB)
-  duration = getVideoDuration (infile)
-  videoSegments = getSectionsOfNewVideo (silences, duration)
-
-  videoFilter = getFileContent_videoFilter (videoSegments)
-  audioFilter = getFileContent_audioFilter (videoSegments)
-
-  print ("create new video")
-  ffmpeg_run (infile, videoFilter, audioFilter, outfile)
-
-def printHelp():
-  print ("Usage:")
-  print ("   silence_cutter.py [infile] [optional: outfile] [optional: dB]")
-  print ("   ")
-  print ("        [outfile]")
-  print ("         Default: [infile]_cut")
-  print ("   ")
-  print ("        [dB]")
-  print ("         Default: -30")
-  print ("         A suitable value might be around -50 to -35.")
-  print ("         The lower the more volume will be defined das 'silent'")
-  print ("         -30: Cut Mouse clicks and mouse movent; cuts are very recognizable.")
-  print ("         -35: Cut inhaling breath before speaking; cuts are quite recognizable.")
-  print ("         -40: Cuts are almost not recognizable.")
-  print ("         -50: Cuts are almost not recognizable.")
-  print ("              Cuts nothing, if there is background noise.")
-  print ("         ")
-  print ("")
-  print ("Dependencies:")
-  print ("          ffmpeg")
-  print ("          ffprobe")
-
-def main():
-  args = sys.argv[1:]
-  if (len(args) < 1):
-    printHelp()
-    return
-
-  if (args[0] == "--help"):
-    printHelp()
-    return
-
-  infile = args[0]
-
-  if (not os.path.isfile (infile)):
-    print ("ERROR: The infile could not be found:\n" + infile)
-    return
-
-  # set default values for optionl arguments
-  tmp = os.path.splitext (infile)
-  outfile = tmp[0] + "_cut" + tmp[1]
-  dB = -30
-
-  if (len(args) >= 2):
-    outfile = args[1]
-
-  if (len(args) >= 3):
-    dB = args[2]
-
-  cut_silences (infile, outfile, dB)
-
-
-if __name__ == "__main__":
-  main()
+  silences = find_silences(infile,dB)
+  duration = get_video_duration(infile)
+  videoSegments = get_sections_of_new_video(silences, duration)
+  videoFilter = get_file_content_video_filter(videoSegments)
+  audioFilter = get_file_content_audio_filter(videoSegments)
+  ffmpeg_run(infile, videoFilter, audioFilter, outfile)

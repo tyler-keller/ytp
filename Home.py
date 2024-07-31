ee0f7812dc9ec71d31ee50f733db5f74b550f2d9
Tyler Keller <tyler.c.keller@gmail.com>
2024-07-30 19:35:39 -0600

    update

--- Home.py
+++ Home.py
@@ -11,9 +11,17 @@ def download_video(url):
     vid_id = parse_qs(parsed_url.query)['v'][0]
     command = ['yt-dlp', url, '-o', f'./videos/%(id)s', '-f', 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best']
     # st.write_stream(subprocess.call(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+    progess = st.text('Downloading...')
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
-    st.write(process.stdout.read())
+    progess.text(str(process.stdout.read()))
     return vid_id
+
+def remove_silence(vid_id):
+    command = ['unsilence', f'./videos/{vid_id}.mp4', f'./videos/{vid_id}_cut_silence.mp4']
+    # st.write_stream(subprocess.call(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+    progess = st.text('Removing silence...')
+    subprocess.call(command, stdout=subprocess.PIPE)
+    # progess.text(str(process.stdout.read()))
 
 form = st.form(key='youtube-url')
 
@@ -24,3 +32,7 @@ with form:
 vid_id = download_video(url)
 
 st.video(f'./videos/{vid_id}.mp4')
+
+remove_silence(vid_id)
+
+st.video(f'./videos/{vid_id}_cut_silence.mp4')
--- slience_cutter.py
+++ slience_cutter.py
@@ -9,7 +9,7 @@ def find_silences(filename, dB = -35):
       even elements (0,2,4, ...) denote silence start time
       uneven elements (1,3,5, ...) denote silence end time
   """
-  command = ["ffmpeg", "-i", filename, "-af", "silencedetect=n=" + str (dB) + "dB:d=1", "-f", "null", "-"]
+  command = ["ffmpeg", "-i", filename, "-af", "silencedetect=n=" + str(dB) + "dB:d=1", "-f", "null", "-"]
   output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   s = str(output)
   lines = s.split("\\n")
@@ -18,29 +18,29 @@ def find_silences(filename, dB = -35):
   for line in lines:
     if ("silencedetect" in line):
         words = line.split(" ")
-        for i in range (len(words)):
+        for i in range(len(words)):
             if ("silence_start" in words[i]):
-              time_list.append (float(words[i+1]))
+              time_list.append(float(words[i+1]))
             if "silence_end" in words[i]:
-              time_list.append (float (words[i+1]))
-  silence_section_list = list (zip(*[iter(time_list)]*2))
+              time_list.append(float(words[i+1]))
+  silence_section_list = list(zip(*[iter(time_list)]*2))
 
   #return silence_section_list
   return time_list
 
 def get_video_duration(filename:str) -> float:
   command = ["ffprobe","-i",filename,"-v","quiet","-show_entries","format=duration","-hide_banner","-of","default=noprint_wrappers=1:nokey=1"]
-  output = subprocess.run (command, stdout=subprocess.PIPE)
+  output = subprocess.run(command, stdout=subprocess.PIPE)
   s = str(output.stdout, "UTF-8")
-  return float (s)
+  return float(s)
 
-def get_sections_of_new_video (silences, duration):
+def get_sections_of_new_video(silences, duration):
   """Returns timings for parts, where the video should be kept"""
   return [0.0] + silences + [duration]
 
 def ffmpeg_filter_getSegmentFilter(videoSectionTimings):
   ret = ""
-  for i in range (int (len(videoSectionTimings)/2)):
+  for i in range(int(len(videoSectionTimings)/2)):
     start = videoSectionTimings[2*i]
     end   = videoSectionTimings[2*i+1]
     ret += "between(t," + str(start) + "," + str(end) + ")+"
@@ -50,38 +50,38 @@ def ffmpeg_filter_getSegmentFilter(videoSectionTimings):
 
 def get_file_content_video_filter(videoSectionTimings):
   ret = "select='"
-  ret += ffmpeg_filter_getSegmentFilter (videoSectionTimings)
+  ret += ffmpeg_filter_getSegmentFilter(videoSectionTimings)
   ret += "', setpts=N/FRAME_RATE/TB"
   return ret
 
 def get_file_content_audio_filter(videoSectionTimings):
   ret = "aselect='"
-  ret += ffmpeg_filter_getSegmentFilter (videoSectionTimings)
+  ret += ffmpeg_filter_getSegmentFilter(videoSectionTimings)
   ret += "', asetpts=N/SR/TB"
   return ret
 
-def write_file (filename, content):
-  with open (filename, "w") as file:
-    file.write (str(content))
+def write_file(filename, content):
+  with open(filename, "w") as file:
+    file.write(str(content))
 
 def ffmpeg_run(file, videoFilter, audioFilter, outfile):
   # prepare filter files
-  vFile = tempfile.NamedTemporaryFile (mode="w", encoding="UTF-8", prefix="silence_video")
-  aFile = tempfile.NamedTemporaryFile (mode="w", encoding="UTF-8", prefix="silence_audio")
+  vFile = tempfile.NamedTemporaryFile(mode="w", encoding="UTF-8", prefix="silence_video")
+  aFile = tempfile.NamedTemporaryFile(mode="w", encoding="UTF-8", prefix="silence_audio")
   videoFilter_file = vFile.name #"/tmp/videoFilter" # TODO: replace with tempfile
   audioFilter_file = aFile.name #"/tmp/audioFilter" # TODO: replace with tempfile
   write_file(videoFilter_file, videoFilter)
   write_file(audioFilter_file, audioFilter)
-  command = ["ffmpeg","-i",file,
+  command = ["ffmpeg", '-n',"-i",file,
               "-filter_script:v",videoFilter_file,
               "-filter_script:a",audioFilter_file,
               outfile]
-  subprocess.run (command)
+  subprocess.run(command)
   vFile.close()
   aFile.close()
 
 
-def cut_silences(infile, outfile, dB = -35):
+def cut_silences(infile, outfile, dB=-35):
   silences = find_silences(infile,dB)
   duration = get_video_duration(infile)
   videoSegments = get_sections_of_new_video(silences, duration)
